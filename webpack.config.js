7a3f9db74d6815fea89cac8b2704a0ab1d8f432d
Clare Hutchings <clareh@socialtables.com>
2016-03-27 15:59:31 -0400

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,43 @@
-module.exports = {
-    entry: "./src/client/entry.js",
+var path = require("path");
+
+var config = {
+    context: path.join(__dirname, "src"),
+    node: {
+        process: false
+    },
+    resolve: {
+        root: path.join(__dirname, "src"),
+        extensions: ["", ".js"],
+        alias: {
+            react$: path.join(__dirname, "node_modules", "react"),
+            "react-dom$": path.join(__dirname, "node_modules", "react-dom")
+        },
+    },
+    entry: {
+        BlockParty: "./client/app.js"
+    },
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: path.join(__dirname, "public", "assets"),
+        filename: "[name].js",
+        library: ["BlockParty", "[name]"],
+        publicPath: "/assets/"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            {
+                test: /\.js$/,
+                loader: "babel",
+                include: path.join(__dirname, "src"),
+                query: {
+                    presets: ["react", "es2015"]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            }
         ]
     }
 };
+
+module.exports = config;
