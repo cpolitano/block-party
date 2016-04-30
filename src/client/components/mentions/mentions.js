23942457fe7d8af66c7b90ce5a5e5a30af7e9f88
Clare Hutchings <clareh@socialtables.com>
2016-04-30 12:33:41 -0400

    mentions component

--- src/client/components/mentions/mentions.js
+++ src/client/components/mentions/mentions.js
@@ -3,10 +3,23 @@ import { Component } from "react";
 import {
 	getMentions
 } from "../../actions/async/mentions"
+require("./mentions.less");
 
 class Mentions extends Component {
 
+	renderTweets(tweet) {
+
+		return (
+			<li>
+				{tweet.user.screen_name}<br/>
+				{tweet.text}<br/>
+				{tweet.created_at}
+			</li>
+		)
+	}
+
 	render() {
+		console.log("props", this.props);
 
 		return (
 			<div className="mentions">
@@ -15,14 +28,20 @@ class Mentions extends Component {
 					onClick={this.props.onMentionsClick}>
 					Get Mentions
 				</div>
+				<ul>
+					{this.props.mentions.map(this.renderTweets, this)}
+				</ul>
 			</div>
 		)
 	}
 }
 
 const mapStateToProps = (state) => {
+	let mentions = state.mentions.mentions || [];
+	console.log("state", state);
+
 	return {
-		// 
+		mentions
 	}
 }
 
