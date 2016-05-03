62478b49204b9b1e9db0201b0e0835b7ce96fb6d
Clare Hutchings <clareh@socialtables.com>
2016-05-02 21:20:41 -0400

    blocks styles

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,3 +1,4 @@
 export { default as App } from "./app/app";
 export { default as Welcome } from "./welcome/welcome";
 export { default as Mentions } from "./mentions/mentions";
+export { default as Blocks } from "./blocks/blocks";
